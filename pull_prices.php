673114ce9531fd5acbb11427fbbbb10b432103fe
German Gonzalez <german.gonzalez@zumba.com>
2017-12-08 02:34:29 -0500

    new

--- pull_prices.php
+++ pull_prices.php
@@ -1,9 +1,13 @@
 <?php
 $rr = set_time_limit(0);
+
 var_dump($rr);
 
-
+echo "Runing ... bowhead:websocket_bitfinex. \n\n";
 exec("php artisan bowhead:websocket_bitfinex > /dev/null &");
+
+echo "Runing ... bowhead:websocket_bitfinex_eth. \n\n";
+exec("php artisan bowhead:websocket_bitfinex_eth > /dev/null &");
 
 
 ?>
