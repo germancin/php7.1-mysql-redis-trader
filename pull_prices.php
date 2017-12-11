e6bba3c0c2593f045b5c5901284a3f915e407f01
german gonzalez <gcomlnk@gmail.com>
2017-12-11 12:50:35 -0500

    adding webhook listener to push and pull automatically

--- pull_prices.php
+++ pull_prices.php
@@ -1,13 +1,20 @@
 <?php
-$rr = set_time_limit(0);
+set_time_limit(0);
 
-var_dump($rr);
+error_log("Runing at: " . date('l jS \of F Y h:i:s A') . " \n\n");
 
 echo "Runing ... bowhead:websocket_bitfinex. \n\n";
+
 exec("php artisan bowhead:websocket_bitfinex > /dev/null &");
 
+error_log("Runing ... bowhead:websocket_bitfinex. \n\n");
+
 echo "Runing ... bowhead:websocket_bitfinex_eth. \n\n";
+
 exec("php artisan bowhead:websocket_bitfinex_eth > /dev/null &");
 
+error_log("Runing ... bowhead:websocket_bitfinex_eth. \n");
+
+error_log("*********************************************\n\n");
 
 ?>
